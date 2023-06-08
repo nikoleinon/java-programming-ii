d96eae17f22cd5055aa3ee826fa1463daf5c1b03
Niko Leinonen <nikoleinonen@hotmail.com>
2023-06-08 14:02:36 +0300

    .

---
+++ part10-Part10_04.Divisible/src/main/java/Divisible.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Divisible {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(-17);
+        numbers.add(-5);
+        numbers.add(7);
+
+        ArrayList<Integer> divisible = divisible(numbers);
+
+        divisible.stream()
+                .forEach(luku -> System.out.println(luku));
+    }
+
+    public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
+        ArrayList<Integer> divisibleNumbers = new ArrayList<>();
+
+        for (Integer number : numbers) {
+            if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0) {
+                divisibleNumbers.add(number);
+            }
+        }
+
+        return divisibleNumbers;    }
+
+}
