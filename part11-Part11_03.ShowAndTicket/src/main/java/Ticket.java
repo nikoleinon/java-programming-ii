e816680d18642ca83474121428a77c46bb9c4fcc
Niko Leinonen <nikoleinonen@hotmail.com>
2023-06-12 15:05:35 +0300

    Init

---
+++ part11-Part11_03.ShowAndTicket/src/main/java/Ticket.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Ticket {
+    private int seat;
+    private int code;
+    private Show show;
+
+    public Show getShows(){
+        return this.show;
+    }
+    
+}
