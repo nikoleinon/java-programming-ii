739952d470086b1c93748f36e8a784bdefe7bca8
Niko Leinonen <nikoleinonen@hotmail.com>
2023-06-12 14:07:42 +0300

    Init

---
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -0,0 +1,29 @@
+
+
+public class Checker {
+
+    public Checker(){
+
+    }
+
+    public boolean isDayOfWeek(String string) {
+        if(string.matches("mon|tue|wed|thu|fri|sat|sun")){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean allVowels(String string) {
+        if(string.matches("[aeiouAEIOU]*")){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean timeOfDay(String string) {
+        if(string.matches("([01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d")){
+            return true;
+        }
+        return false;
+    }
+}
