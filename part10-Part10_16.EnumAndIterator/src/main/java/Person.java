a20e436142066ec505f09873d3716a5e917a34de
Niko Leinonen <nikoleinonen@hotmail.com>
2023-06-12 14:23:24 +0300

    Init

---
+++ part10-Part10_16.EnumAndIterator/src/main/java/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    
+    private String name;
+    private Education education;
+
+    public Person(String name, Education education){
+        this.name = name;
+        this.education = education;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Education getEducation() {
+        return education;
+    }
+    public void setEducation(Education education) {
+        this.education = education;
+    }
+    public String toString(){
+        return this.name + ", " + this.education;
+    }
+}
