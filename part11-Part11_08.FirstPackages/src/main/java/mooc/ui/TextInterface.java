5471773949f012793cddecc4ceed79d29ca8c8c1
Niko Leinonen <nikoleinonen@hotmail.com>
2023-06-13 08:41:37 +0300

    Init

---
+++ part11-Part11_08.FirstPackages/src/main/java/mooc/ui/TextInterface.java
@@ -0,0 +1,10 @@
+package mooc.ui;
+
+public class TextInterface implements UserInterface {
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+    System.out.println("Updating UI");    
+    }
+}
