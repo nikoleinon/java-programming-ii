7226bd9301224eda44c9d694a75737760368e778
Niko Leinonen <nikoleinonen@hotmail.com>
2023-06-15 09:47:34 +0300

    Init

---
+++ part12-Part12_04.List/src/main/java/List.java
@@ -0,0 +1,51 @@
+public class List<Type> {
+
+    private Type[] values;
+    private int firstFreeIndex;
+
+    public List() {
+
+        this.values = (Type[]) new Object[10];
+        this.firstFreeIndex = 0;
+    }
+
+    public void add(Type value) {
+        if (this.firstFreeIndex == this.values.length) {
+            grow();
+        }
+        this.values[this.firstFreeIndex] = value;
+        this.firstFreeIndex++;
+
+    }
+
+    private void grow() {
+        int newSize = this.values.length + this.values.length / 2;
+        Type[] newValues = (Type[]) new Object[newSize];
+        for (int i = 0; i < this.values.length; i++) {
+            newValues[i] = this.values[i];
+        }
+
+        this.values = newValues;
+    }
+
+    public boolean contains(Type value) {
+        for (int i = 0; i < this.firstFreeIndex; i++) {
+            if (this.values[i].equals(value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void remove(Type value) {
+        for (int i = 0; i < this.firstFreeIndex; i++) {
+            if (value == this.values[i] || this.values[i].equals(value)) {
+                this.values[i] = null;
+                this.firstFreeIndex--;
+                return;
+            }
+        }
+    }
+
+}
